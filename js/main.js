159589d6aba38b57fdcb576a1dd84470a484fd47
Caleb <95381786+calebeffiom@users.noreply.github.com>
2024-04-15 16:33:42 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let names = document.getElementById("name").value
+let email = document.getElementById("email").value
+let phonenumber = document.getElementById("number").value
+let message = document.getElementById("message").value
+var mail = document.getElementById("submit");
+
+
+const send = ()=>{
+    mail.href = `mailto:calebeffiom@gmail.com?subject=Enquires?body=Name:${names}%0d%0aEmail:${email}%0d%0aPhonenumber:${phonenumber}%0d%0Message:${message}%0d%0a`;
+}
+mail.addEventListener("click",()=>{send()})
