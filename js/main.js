5c40a0bbd13f7e6f727be9ff6e1271b22a44f201
Caleb <95381786+calebeffiom@users.noreply.github.com>
2024-04-15 16:44:13 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let names = document.getElementById("name").value
+let names = document.getElementById("name")
 let email = document.getElementById("email").value
 let phonenumber = document.getElementById("number").value
 let message = document.getElementById("message").value
@@ -6,6 +6,6 @@ var mail = document.getElementById("submit");
 
 
 const send = ()=>{
-    mail.href = `mailto:calebeffiom@gmail.com?subject=Enquires?body=Name:${names}%0d%0aEmail:${email}%0d%0aPhonenumber:${phonenumber}%0d%0Message:${message}%0d%0a`;
+    mail.href = `mailto:calebeffiom@gmail.com?subject=Enquires&body=Name:${names.value}%0d%0aEmail:${email}%0d%0aPhonenumber:${phonenumber}%0d%0Message:${message}%0d%0a`;
 }
 mail.addEventListener("click",()=>{send()})
