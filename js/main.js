1b216a49fd66ba855bed9a77755fafe6f5b8ae08
Caleb <95381786+calebeffiom@users.noreply.github.com>
2024-04-19 14:17:42 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ let names = document.getElementById("name")
 let email = document.getElementById("email")
 let phonenumber = document.getElementById("number")
 let message = document.getElementById("message")
-var mail = document.getElementById("submit-form")
-var btn = document.getElementById("submit-btn")
+var mail = document.getElementById("submit-btn")
+// var btn = document.getElementById("submit-btn")
 
 
 const send = ()=>{
-    mail.action = `mailto:calebeffiom974@gmail.com?subject=Enquires`;
+    mail.href = `mailto:edulauchlabs@gmail.com?subject=Enquires&body=Name:%20${names.value}%0D%0AEmail:%20${email.value}%0D%0APhoneNumber:%20${phonenumber.value}%0D%0AMessage:%20${message.value}`;
 }
-btn.addEventListener("click",()=>{send()})
+mail.addEventListener("click",()=>{send()})
